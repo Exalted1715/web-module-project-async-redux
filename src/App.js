82c73752b3b32f6fffa7317d84f06de4127324c9
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-02-06 15:57:38 -0700

    updated axios return web call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import { fetchStart } from './actions';
+import { useEffect } from 'react';
 import CurrentWeather from './components/CurrentWeather';
 import WeatherForm from './components/WeatherForm';
 import WeatherList from './components/WeatherList'; 
 import './App.css';
-
+import axios from 'axios';
 
 function App(props) {
  
 const {loading, error} = props
 
-
+useEffect(() => {
+  props.fetchStart();
+  axios.get('https://api.weatherbit.io/v2.0/current?&postal_code=83709&key=f899a6ac8b82427bb2ed20e594ccad85')
+    .then(res =>{
+      console.log(res)
+    })
+}, []);
 
   return (
     <div className="App">
@@ -35,4 +43,10 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+const mapActionsToProps = () => {
+  return{
+    fetchStart
+  }
+}
+
+export default connect(mapStateToProps, {fetchStart})(App);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,13 +1,21 @@
 import data from './../data/weatherData';
 
+import { FETCH_START } from '../actions';
+
 const intialState = {
-   weather: data,
+   weather: [],
    loading: false,
    error: '' 
 }
 
 const reducer = (state = intialState, action) => {
     switch(action.type){
+        case FETCH_START:
+        return{
+            ...state,
+            loading: true,
+            error: ''
+        }
         default:
             return(state);
     }
